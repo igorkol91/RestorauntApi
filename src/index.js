460224d6dd965f47b1ff9c2ab0a44f9b39b58843
Igor.Koloski <koloski_igor@hotmail.com>
2021-09-07 09:58:25 +0200

    Add responsive 3 sizes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './main.css';
 import getData from './modules/dom.js';
 
+// this is just to avoid linter error for now
 console.log(getData);
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,9 +1,7 @@
-const restorauntObjects = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 const main = document.querySelector('main');
 
+// I left this one for you. Here you can take the id from the object we are displaying
 // data.categories[0].idCategory
-// console.log(data.categories[0].strCategory)
-// data.categories.forEach((elem) => displayData(elem))
 
 const displayData = (data) => {
   // create elements for a menu item
@@ -11,8 +9,8 @@ const displayData = (data) => {
   const menuImg = document.createElement('img');
   const menuHeadline = document.createElement('h3');
   const menuText = document.createElement('p');
-  // put menuDiv and elements classes
-  menuDiv.classList = 'menuDiv';
+  // put classes
+  menuDiv.classList = 'menuDiv col-sm-12 col-md-6 col-lg-4';
   // put values to every element
   menuHeadline.innerHTML = data.strCategory;
   menuImg.src = data.strCategoryThumb;
@@ -21,7 +19,7 @@ const displayData = (data) => {
   main.appendChild(menuDiv);
 };
 
-const getData = fetch(restorauntObjects)
+const getData = fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
   .then((response) => response.json())
   .then((data) => data.categories.forEach((element) => {
     displayData(element);
