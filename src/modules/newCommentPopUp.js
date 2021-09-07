ed5a9be89a7ed0c74410e6ffae4dfeda2a2aa9d9
HENRY-KC <henrykc24@yahoo.com>
2021-09-07 15:23:09 +0100

    Integrate get comment to newCoomentPopUp funtion

--- src/modules/newCommentPopUp.js
+++ src/modules/newCommentPopUp.js
@@ -1,6 +1,8 @@
 import getData from "./dom";
+import getComments from "./getComments";
+import postComment from "./postComment";
 
-const newCommentPopUp = (foodObject, main, menuDiv) => {
+const newCommentPopUp = async (foodObject, main, menuDiv) => {
   /* 
   {idCategory: '1', strCategory: 
   'Beef', strCategoryThumb: 'https://www.themealdb.com/images/category/beef.png', 
@@ -11,9 +13,12 @@ const newCommentPopUp = (foodObject, main, menuDiv) => {
   strCategoryDescription: "Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times.[1] Beef is a source of high-quality protein and essential nutrients.[2]
   "strCategoryThumb: "https://www.themealdb.com/images/category/beef.png"[[Prototype]]: Object
   */
+
+  //IvP42xNcmZ7sT5rp87wL    App id for comments
+
   const { idCategory, strCategoryThumb, strCategory, strCategoryDescription } =
     foodObject;
-
+  
   const overlay = document.createElement("section");
   overlay.className = "overlay";
 
@@ -23,11 +28,11 @@ const newCommentPopUp = (foodObject, main, menuDiv) => {
   const closeButton = document.createElement("button");
   closeButton.className = "close";
   closeButton.innerHTML = "&times;";
-  closeButton.addEventListener('click', () => {
-    document.querySelector('.overlay').classList.add('hide');
-    main.removeChild(document.querySelector('.overlay'));
-    document.querySelector('body').classList.remove('stop-scrolling');
-    menuDiv.classList.remove('hide');
+  closeButton.addEventListener("click", () => {
+    document.querySelector(".overlay").classList.add("hide");
+    main.removeChild(document.querySelector(".overlay"));
+    document.querySelector("body").classList.remove("stop-scrolling");
+    menuDiv.classList.remove("hide");
   });
 
   const image = document.createElement("img");
@@ -45,16 +50,27 @@ const newCommentPopUp = (foodObject, main, menuDiv) => {
 
   const numberOfCommentsContainer = document.createElement("h2");
   numberOfCommentsContainer.className = "num-of-comments";
-  numberOfCommentsContainer.textContent = `Comments(${2})`;
-
+  
   const commentsContainer = document.createElement("div");
   commentsContainer.className = "comments-container";
+  
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/IvP42xNcmZ7sT5rp87wL/comments/?item_id=${idCategory}`;
+  
+  const allComments = await getComments(url);
+  console.log(allComments);
+  numberOfCommentsContainer.textContent = `Comments(${!allComments.error && allComments.length || 0})`;
 
-  const comment = document.createElement("p");
-  comment.className = "comment";
-  comment.textContent = `Date Name: The comment`;
-
-  commentsContainer.appendChild(comment);
+  !allComments.error && allComments.forEach((each) => {
+    const comment = document.createElement("p");
+    comment.className = "comment";
+    commentsContainer.appendChild(comment);
+    /*
+    "comment": "This is nice!",
+        "creation_date": "2021-01-10",
+        "username": "John"
+    */
+    comment.innerHTML = `<strong>${each.creation_date} ${each.username}</strong>: ${each.comment}`;
+  });
 
   const newCommentHeading = document.createElement("h2");
   newCommentHeading.className = "new-comment-heading";
@@ -73,6 +89,28 @@ const newCommentPopUp = (foodObject, main, menuDiv) => {
   insightBox.placeholder = "Your insights";
 
   const commentButton = document.createElement("button");
+  commentButton.addEventListener("click", async () => {
+    const { value: name } = document.querySelector(".name");
+    const { value: insight } = document.querySelector(".insight");
+
+    if (name && insight) {
+      const url =
+        "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/IvP42xNcmZ7sT5rp87wL/comments/";
+      const body = {
+        item_id: idCategory,
+        username: name,
+        comment: insight,
+      };
+      try {
+        const result = await postComment(body, url);
+        console.log("After posting", result);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.log(88888);
+    }
+  });
   commentButton.className = "comment-button";
   commentButton.textContent = "Comment";
 
