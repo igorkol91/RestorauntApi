e15c18c3c7b5b52491a1cddc8d44de107e1fba3e
Igor.Koloski <koloski_igor@hotmail.com>
2021-09-07 15:00:27 +0200

    Add conflict resolve

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,9 +1,7 @@
-const restorauntObjects = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 const main = document.querySelector('main');
 
+// I left this one for you. Here you can take the id from the object we are displaying
 // data.categories[0].idCategory
-// console.log(data.categories[0].strCategory)
-// data.categories.forEach((elem) => displayData(elem))
 
 const displayData = (data) => {
   // create elements for a menu item
@@ -11,17 +9,30 @@ const displayData = (data) => {
   const menuImg = document.createElement('img');
   const menuHeadline = document.createElement('h3');
   const menuText = document.createElement('p');
-  // put menuDiv and elements classes
-  menuDiv.classList = 'menuDiv';
+  const likesAndComments = document.createElement('div');
+  const commentsBtn = document.createElement('button');
+  const likesCount = document.createElement('p');
+  const likesBtn = document.createElement('button');
+  // put classes and values
+  menuDiv.classList = 'menuDiv col-sm-12 col-md-6 col-lg-4 my-4';
+  menuText.classList = 'menuText';
+  likesAndComments.classList = 'likesComments';
+  commentsBtn.classList = 'comment-btn';
+  commentsBtn.innerText = 'Comment';
+  likesCount.classList = 'likesCountP';
+  likesCount.innerText = '0';
+  likesBtn.classList = 'px-5';
+  likesBtn.innerText = 'Like';
   // put values to every element
   menuHeadline.innerHTML = data.strCategory;
   menuImg.src = data.strCategoryThumb;
   menuText.innerHTML = data.strCategoryDescription;
-  menuDiv.append(menuImg, menuHeadline, menuText);
+  likesAndComments.append(commentsBtn, likesCount, likesBtn);
+  menuDiv.append(menuImg, menuHeadline, menuText, likesAndComments);
   main.appendChild(menuDiv);
 };
 
-const getData = fetch(restorauntObjects)
+const getData = fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
   .then((response) => response.json())
   .then((data) => data.categories.forEach((element) => {
     displayData(element);
