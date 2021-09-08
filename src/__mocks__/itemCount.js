bac8857033e9484271eb1ae09947e7651550a741
Igor.Koloski <koloski_igor@hotmail.com>
2021-09-08 15:32:36 +0200

    Add tests for itemCount function

--- src/__mocks__/itemCount.js
+++ src/__mocks__/itemCount.js
@@ -1,17 +1,9 @@
-const meals = document.createElement('main');
-meals.id = 'mealsID';
+const meals = document.createElement('label');
 
-const countMeals = () => {
-  const container = `<main class='main-container'>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    </main>`;
+const countMeals = (container) => {
   const mealsNumber = Array.from(container.childNodes).length;
   meals.innerText = `${mealsNumber} Meals`;
-  console.log(mealsNumber);
-  return mealsNumber;
+  return parseInt(Array.from(container.childNodes).length, 10);
 };
 
 export default countMeals;
--- src/modules/itemCount.js
+++ src/modules/itemCount.js
@@ -1,9 +1,8 @@
-const meals = document.querySelector('#mealsID');
+const meals = document.createElement('p');
 
 const countMeals = (container) => {
   const mealsNumber = Array.from(container.childNodes).length;
   meals.innerText = `${mealsNumber} Meals`;
-  return mealsNumber;
 };
 
 export default countMeals;
